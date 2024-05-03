f56e266e67c47b2e9e833d6af2b4303bf3830aee
prabinnnn <prabinbhandari120@gmail.com>
2024-05-03 14:12:06 +0545

    add header

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -4,7 +4,11 @@ import "../header/header.css";
 const Header = () => {
   return (
     <header>
-      <div className="container-fluid"></div>
+      <div className="container-fluid">
+        <div class="row">
+          <div class="col-sm-2"></div>
+        </div>
+      </div>
     </header>
   );
 };
