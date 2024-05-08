4dd8d03f152a5353aac56163906b5cec8020392a
prabinnnn <prabinbhandari120@gmail.com>
2024-05-08 17:16:17 +0545

    add header

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -6,7 +6,9 @@ const Header = () => {
     <header>
       <div className="container-fluid">
         <div class="row">
-          <div class="col-sm-2"></div>
+          <div class="col-sm-2">
+            <image src={Logo} />
+          </div>
         </div>
       </div>
     </header>
