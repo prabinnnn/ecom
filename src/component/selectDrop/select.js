2d6c11efa7469f501a4cd9840ef05a42f5f6e369
prabinnnn <prabinbhandari120@gmail.com>
2024-05-20 23:13:23 +0545

    add index

--- src/component/selectDrop/select.js
+++ src/component/selectDrop/select.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "../selectDrop/select.css";
 const select = () => {
-  return <div className="SelectDrop"></div>;
+  return (
+    <div className="SelectDrop">
+      <div className="SearchField">
+        <input type="text" />
+      </div>
+    </div>
+  );
 };
 export default select;
