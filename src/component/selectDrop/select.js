6ef40409fa821759a657f36f207499e1a7a6623e
prabinnnn <prabinbhandari120@gmail.com>
2024-06-09 10:49:43 +0545

    add select

--- src/component/selectDrop/select.js
+++ src/component/selectDrop/select.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../selectDrop/select.css";
 const select = () => {
+  const [isOpenSelect,setisOpenSelect]=useState(false)
   return (
-    <div className="selectdrop cursor poistion-relative">
+    <div className="selectdropwrapper cursor poistion-relative">
                 <span className="openSelect">AllCatergories</span>
-    <div className="SelectDrop">
-      <div className="SearchField">
-        <input type="text" />
-      </div>
-      <ul className="SearchResults">
-        <li>Milk and Dairies</li>
-        <li>Milk and Dairies</li>
-        <li>Milk and Dairies</li>
-        <li>Milk and Dairies</li>
-        <li>Milk and Dairies</li>
-        <li>Milk and Dairies</li>
-        <li>Milk and Dairies</li>
-        <li>Milk and Dairies</li>
-        <li>Milk and Dairies</li>
-        <li>Milk and Dairies</li>
-      </ul>
-    </div>
+                {
+                  isOpenSelect === true && 
+                  <div className="SelectDrop">
+                  <div className="SearchField">
+                    <input type="text" />
+                  </div>
+                  <ul className="SearchResults">
+                    <li>Milk and Dairies</li>
+                    <li>Milk and Dairies</li>
+                    <li>Milk and Dairies</li>
+                    <li>Milk and Dairies</li>
+                    <li>Milk and Dairies</li>
+                    <li>Milk and Dairies</li>
+                    <li>Milk and Dairies</li>
+                    <li>Milk and Dairies</li>
+                    <li>Milk and Dairies</li>
+                    <li>Milk and Dairies</li>
+                  </ul>
+                </div>
+                }
+  
     </div>
   );
 };
